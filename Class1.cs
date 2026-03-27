aa70df2c6118374a26af972cb223799d545bf699
milly1110ning-dot <milly.1110ning@gmail.com>
2026-03-27 12:11:44 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public struct Student
+{
+	public int StdID;
+
+	private string Name;
+
+
+
+	public Student(int StdID,string Name)
+	{
+		this.StdID=StdID;
+		this.Name=Name;
+	}
+
+
+	public string GetStudent()
+	{
+		string str;
+		str=StdID.ToString();
+		str += "" + Name;
+
+		return str;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Class_0327_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student joe = new Student(1,"AAA");
+            Student jane = new Student(2, "BBB");
+
+
+            Console.WriteLine(joe.GetStudent());
+            Console.WriteLine(jane.GetStudent());
+
+        }
+    }
+}
